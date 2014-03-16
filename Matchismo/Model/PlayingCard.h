356f87b0c9143ac7de3f65acedf86bdb666cadec
Alejandro Machado <alemacgo@gmail.com>
2014-03-16 16:11:04 +0000

    Add PlayingCard and PlayingCardDeck subclasses

---
+++ Matchismo/Model/PlayingCard.h
@@ -0,0 +1,19 @@
+//
+//  PlayingCard.h
+//  Matchismo
+//
+//  Created by Alejandro Machado on 14/3/16.
+//  Copyright (c) 2014 M-ITI. All rights reserved.
+//
+
+#import "Card.h"
+
+@interface PlayingCard : Card
+
+@property (strong, nonatomic) NSString *suit;
+@property (nonatomic) NSUInteger rank;
+
++ (NSArray *)validSuits;
++ (NSUInteger)maxRank;
+
+@end
---
+++ Matchismo/Model/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  Matchismo
+//
+//  Created by Alejandro Machado on 14/3/16.
+//  Copyright (c) 2014 M-ITI. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
