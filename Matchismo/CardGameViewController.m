8f780b89098d8e3fea7a211eb9188ea43c64d425
Alejandro Machado <alemacgo@gmail.com>
2014-03-16 14:43:33 +0000

    L02 1:01:49 Add IBAction to flip card from front to back

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -14,16 +14,11 @@
 
 @implementation CardGameViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+- (IBAction)touchCardButton:(UIButton *)sender {
+    UIImage *cardImage = [UIImage imageNamed:@"cardback"];
+    [sender setBackgroundImage:cardImage forState:UIControlStateNormal];
+    [sender setTitle:@"" forState:UIControlStateNormal];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
