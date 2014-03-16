1db10842018df7b2d36813a1108afa22363cf4f2
Alejandro Machado <alemacgo@gmail.com>
2014-03-16 15:15:29 +0000

    L02 END Add an empty Card class

---
+++ Matchismo/Model/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  Matchismo
+//
+//  Created by Alejandro Machado on 14/3/16.
+//  Copyright (c) 2014 M-ITI. All rights reserved.
+//
+
+@import Foundation;
+
+@interface Card : NSObject
+
+@end
---
+++ Matchismo/Model/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  Matchismo
+//
+//  Created by Alejandro Machado on 14/3/16.
+//  Copyright (c) 2014 M-ITI. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
