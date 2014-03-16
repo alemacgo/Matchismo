1d9ae4a83ca6e984b15458e2a82f7ac438fe3768
Alejandro Machado <alemacgo@gmail.com>
2014-03-16 16:29:07 +0000

    L03 09:10 Stop incrementing flipCount when out of cards

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -37,6 +37,7 @@
         [sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
                           forState:UIControlStateNormal];
         [sender setTitle:@"" forState:UIControlStateNormal];
+        self.flipCount++;
     }
     else {
         Card* card = [self.deck drawRandomCard];
@@ -44,9 +45,9 @@
             [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
                               forState:UIControlStateNormal];
             [sender setTitle:card.contents forState:UIControlStateNormal];
+            self.flipCount++;
         }
     }
-    self.flipCount++;
 }
 
 
